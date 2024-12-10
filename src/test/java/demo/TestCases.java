38cb3bc111f8f0177a02eca33ee5e5bccff8fb97
kashyapmk-ind <kashyapmk.ind@gmail.com>
2024-12-11 03:56:13 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -6,10 +6,12 @@ import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -26,6 +28,60 @@ public class TestCases {
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
 
+     @Test
+     public void testCase01(){
+        
+        Wrappers wrap = new Wrappers(driver);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        //navigate to www.flipkart.com
+        driver.get("https://www.google.com/");
+        wrap.NavigateToUrl("https://www.flipkart.com/");
+        //search for washing machine
+        wrap.searchAndEnter("Washing Machine");
+        //set popularity
+        wrap.setPopularity();
+        //count items less than or equal to 4 star rating
+        wrap.countItemsLessThan(4);
+        
+     }
+
+     @Test
+     public void testCase02(){
+
+        Wrappers wrap = new Wrappers(driver);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        //navigate to www.flipkart.com
+        driver.get("https://www.google.com/");
+        wrap.NavigateToUrl("https://www.flipkart.com/");
+        //search for iPhone
+        wrap.searchAndEnter("iPhone");
+        //print titles and discount % of all Items with discount Greater than 17%
+        wrap.getTitlesAndDiscounts(17);
+
+     }
+
+     @Test
+     public void testCase03(){
+
+        Wrappers wrap = new Wrappers(driver);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        //navigate to www.flipkart.com
+        driver.get("https://www.google.com/");
+        wrap.NavigateToUrl("https://www.flipkart.com/");
+        //search for Coffee Mug
+        wrap.searchAndEnter("Coffee Mug");
+        //set customer rating to 4 & above
+        wrap.setCustomerRatings(4);
+        //print the title and imageURL of 5 items with highest number of reviews
+        wrap.getTitleAndImageURL(5);
+
+     }
+
+
+     
      
     /*
      * Do not change the provided methods unless necessary, they will help in automation and assessment
@@ -52,6 +108,8 @@ public class TestCases {
 
         driver.manage().window().maximize();
     }
+
+
 
     @AfterTest
     public void endTest()
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -4,19 +4,206 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.text.NumberFormat;
 import java.time.Duration;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+
+    
+     ChromeDriver driver;
+     WebDriverWait wait;
+
+     public Wrappers(ChromeDriver driver){
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+     } 
+
+
+     public void NavigateToUrl(String url){
+        try{
+            if(!(driver.getCurrentUrl().contains("flipkart"))){
+                driver.get(url);
+                wait.until(ExpectedConditions.urlContains("flipkart"));
+                Thread.sleep(2000);
+            }
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        
+        
+
+     }
+
+     public boolean searchAndEnter(String productToSearch){
+        try{
+            WebElement searchBar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='Pke_EE' and contains(@title,'Search for Products')]")));
+            Thread.sleep(2000);
+            searchBar.click();
+            searchBar.clear();
+            Thread.sleep(2000);
+            searchBar.sendKeys(productToSearch);
+
+            WebElement searchIcon = driver.findElement(By.xpath("//button[contains(@title,'Search for Products')]"));
+            searchIcon.click();
+            Thread.sleep(5000);
+            return true;
+
+        }catch(Exception e){
+            return false;
+        }
+        
+     }
+
+     public void setPopularity(){
+        try{
+            WebElement popularity = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class,'sHCOk')]/div[text()='Popularity']")));
+            Thread.sleep(1000);
+            popularity.click();
+            Thread.sleep(2000);
+        }catch(Exception e){
+            System.out.println(e);
+        } 
+
+     }
+
+     public int countItemsLessThan(int ratings){
+        try{
+
+            List<WebElement> Items = driver.findElements(By.xpath("//span[contains(@id,'productRating')]/div[number(text())<= " + ratings + "]"));
+            Thread.sleep(2000);
+            int ItemsCount = Items.size();
+            System.out.println("Items count with rating Lesser than or equal to:   "+ItemsCount);
+            return ItemsCount; 
+
+        }catch(Exception e){
+            System.out.println(e);
+            return 0;
+        }
+     }
+
+     public void getTitlesAndDiscounts(int discount) {
+        try {
+    
+            String productXPath = "//div[@class='UkUFwK']/span[contains(text(), '% off') and number(substring-before(text(), '%')) > "+discount+"]/ancestor::div[@class='yKfJKb row']";
+    
+            // Locate all matching products
+            List<WebElement> products = driver.findElements(By.xpath(productXPath));
+            System.out.println("number of iPhones with discount > 17% :- "+products.size());
+    
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+
+            for (WebElement product : products) {
+                // Extract the title
+                WebElement titleElement = product.findElement(By.xpath(".//div[@class='KzDlHZ']"));
+                js.executeScript("arguments[0].scrollIntoView(true);", titleElement);
+                String title = titleElement.getText();
+    
+                // Extract the discount percentage
+                WebElement discountElement = product.findElement(By.xpath(".//div[@class='UkUFwK']/span[contains(text(), '% off')]"));
+                js.executeScript("arguments[0].scrollIntoView(true);", discountElement);
+                String discountText = discountElement.getText();
+                
+                System.out.println("Product Title: " + title);
+                System.out.println("Discount: " + discountText);
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+
+    public void setCustomerRatings(int rating){
+
+        try{
+            
+            String str = String.valueOf(rating);
+            WebElement custR = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(@class,'qKy') and starts-with(text(),'"+str+"')]/preceding-sibling::div[@class='XqNaEv']")));
+            Thread.sleep(1000);
+            custR.click();
+            Thread.sleep(5000);
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        
+    }
+    public void getTitleAndImageURL(int numOfItems_toDisplay){
+        try{
+            ArrayList<Integer> reviewsInteger = new ArrayList<>();
+        
+            //locate reviews element to get text
+            List<WebElement> numOfReviews = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//span[@class='Wphh3N']"))); //driver.findElements(By.xpath(""));
+            Thread.sleep(2000);
+            
+            //convert string values to Integer values for sorting
+            for(WebElement wb : numOfReviews){
+                String revText = wb.getText();
+                // Remove non-numeric characters
+                String cleanedData = revText.replace(",", "").replace("(", "").replace(")", "");
+                // Convert to integer
+                int revNum = Integer.parseInt(cleanedData);
+                reviewsInteger.add(revNum);
+            }
+            //sort array in descending order
+            Collections.sort(reviewsInteger, Collections.reverseOrder());
+
+            int n = 0;
+            if(reviewsInteger.size()<numOfItems_toDisplay){
+                n = reviewsInteger.size();
+            }else{
+                n = numOfItems_toDisplay;
+            }
+
+            for(int i=0; i<n; i++){
+                int numberR = reviewsInteger.get(i);
+                String revString = "(" + NumberFormat.getNumberInstance(Locale.US).format(numberR) + ")";
+
+                WebElement title = driver.findElement(By.xpath("//span[@class='Wphh3N' and text()='"+revString+"']/ancestor::div[contains(@class,'slAVV')]/a[@class='wjcEIp']"));
+                String Title = title.getAttribute("title");
+
+                WebElement imageLink = driver.findElement(By.xpath("//span[@class='Wphh3N' and text()='"+revString+"']/ancestor::div[contains(@class,'slAVV')]//img[contains(@class,'DByuf')]"));
+                String imgUrl = imageLink.getAttribute("src");
+
+                System.out.println(i+". Coffee Mug with highest reviews : " + revString);
+                System.out.println("Coffee Mug title : " + Title);
+                System.out.println("Coffee Mug image URL : " + imgUrl);
+
+            }
+
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+
+        
+
+        
+    }
+
+
+    
+    
+
+
+     
+
+
+
+
 }
